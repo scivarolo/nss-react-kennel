73d37520bc3d1b8b392391023adad24c7426d19d
Sebastian Civarolo <scivarolo@gmail.com>
2018-11-19 10:48:24 -0600

    Move data to API and fetch data from it

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -7,64 +7,38 @@ import OwnerList from './owners/OwnerList'
 
 class ApplicationViews extends Component {
   // TODO: Pull from API
-  employeesFromAPI = [
-    { id: 1, name: "Jessica Younker" },
-    { id: 2, name: "Jordan Nelson" },
-    { id: 3, name: "Zoe LeBlanc" },
-    { id: 4, name: "Blaise Roberts" }
-  ]
-
-  locationsFromAPI = [
-    { id: 1, name: "Nashville North", address: "500 Circle Way" },
-    { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-  ]
-
-  animalsFromAPI = [
-    { id: 1, name: "Mochi" },
-    { id: 2, name: "Milhouse" },
-    { id: 3, name: "Sawyer" },
-    { id: 4, name: "Jack" },
-    { id: 5, name: "Gwendolyn" }
-  ]
-
-  ownersFromAPI = [
-    { id: 1, name: "Kevin Corn", phone: "123-456-7890" },
-    { id: 2, name: "Sebastian Civarolo", phone: "234-567-8901"},
-    { id: 3, name: "Samantha Merritt", phone: "345-678-9012" },
-    { id: 4, name: "Clint Hime", phone: "456-789-0123" },
-    { id: 5, name: "Mary Hime", phone: "567-890-1234" },
-    { id: 6, name: "Alyssa Nelson", phone: "678-901-2345" },
-    { id: 7, name: "Steven Alves", phone: "789-012-3456" }
-  ]
-
-  animalOwnersFromAPI = [
-    { id: 1, animalId: 1, ownerId: 1 },
-    { id: 2, animalId: 1, ownerId: 2 },
-    { id: 3, animalId: 2, ownerId: 3 },
-    { id: 4, animalId: 3, ownerId: 4 },
-    { id: 5, animalId: 3, ownerId: 5 },
-    { id: 6, animalId: 4, ownerId: 6 },
-    { id: 7, animalId: 5, ownerId: 7 }
-  ]
-
   state = {
-    employees: this.employeesFromAPI,
-    locations: this.locationsFromAPI,
-    animals: this.animalsFromAPI,
-    owners: this.ownersFromAPI,
-    animalOwners: this.animalOwnersFromAPI
+    employees: [],
+    locations: [],
+    animals: [],
+    owners: [],
+    animalOwners: []
+  }
+
+  componentDidMount() {
+    const newState = {}
+    const baseUrl = "http://localhost:5002/"
+
+    fetch(`${baseUrl}locations`)
+      .then(r => r.json())
+      .then(locations => newState.locations = locations)
+      .then(() => fetch(`${baseUrl}animals`))
+        .then(r => r.json())
+        .then(animals => newState.animals = animals)
+      .then(() => fetch(`${baseUrl}employees`))
+        .then(r => r.json())
+        .then(employees => newState.employees = employees)
+      .then(() => fetch(`${baseUrl}owners`))
+        .then(r => r.json())
+        .then(owners => newState.owners = owners)
+      .then(() => fetch(`${baseUrl}animalOwners`))
+        .then(r => r.json())
+        .then(animalOwners => newState.animalOwners = animalOwners)
+      .then(() => this.setState(newState))
   }
 
   render() {
     return (
-
-      // <div>
-      //   <h1>Student Kennels</h1>
-      //   <LocationList locations={this.state.locations} />
-      //   <EmployeeList employees={this.state.employees} />
-      //   <AnimalList animals={this.state.animals} owners={this.state.owners} animalOwners={this.state.animalOwners} />
-      // </div>
-
       <React.Fragment>
         <Route exact path="/" render={() => {
           return <LocationList locations={this.state.locations} />
@@ -75,7 +49,7 @@ class ApplicationViews extends Component {
         <Route path="/employees" render={() => {
           return <EmployeeList employees={this.state.employees} />
         }} />
-        <Route path="/owners" render= {() => {
+        <Route path="/owners" render={() => {
           return <OwnerList owners={this.state.owners} />
         }} />
       </React.Fragment>
